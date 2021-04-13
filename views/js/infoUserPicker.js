7bfc2f09b6fc1a8d0782999cea0c0eff1a5517f4
EnricoAffuso <Enrico.affuso1@protonmail.com>
2021-04-13 23:04:16 +0200

    inizio userInfo section

--- views/js/infoUserPicker.js
+++ views/js/infoUserPicker.js
@@ -1,1 +1,8 @@
-document.getElementById("name").innerHTML = "sasso sassaginese"
+let loggedUser = JSON.parse(sessionStorage.getItem("logged"))
+
+
+document.getElementById("name").innerHTML = String(loggedUser.fullname)
+document.getElementById("email").innerHTML = String(loggedUser.email)
+document.getElementById("username").innerHTML = String(loggedUser.username)
+document.getElementById("cardname").innerHTML = String(loggedUser.payment.cardName)
+document.getElementById("cardnumber").innerHTML = String(loggedUser.fullname)
