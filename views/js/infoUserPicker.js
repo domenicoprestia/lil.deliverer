03169c16688d7f32dabd985802e372d59d50fc54
EnricoAffuso <61048715+EnricoAffuso@users.noreply.github.com>
2021-04-14 10:49:50 +0200

    userPicker 2

--- views/js/infoUserPicker.js
+++ views/js/infoUserPicker.js
@@ -5,4 +5,19 @@ document.getElementById("name").innerHTML = String(loggedUser.fullname)
 document.getElementById("email").innerHTML = String(loggedUser.email)
 document.getElementById("username").innerHTML = String(loggedUser.username)
 document.getElementById("cardname").innerHTML = String(loggedUser.payment.cardName)
-document.getElementById("cardnumber").innerHTML = String(loggedUser.fullname)
+
+let cardNumber =  String(loggedUser.payment.cardNumber)
+let splittedCardNumber = cardNumber.split('') 
+
+if(cardNumber.length == 13){
+    cardNumber = "**********" + splittedCardNumber[10] + splittedCardNumber[11] + splittedCardNumber[12]     
+}
+else{
+    cardNumber = "*************" + splittedCardNumber[13] + splittedCardNumber[14] + splittedCardNumber[15]
+}
+
+document.getElementById("cardnumber").innerHTML = cardNumber
+
+let town = String(loggedUser) //continuare da qui 
+
+document.getElementById("town") = town
