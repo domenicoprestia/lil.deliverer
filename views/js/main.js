440094c6bad46450513cca706b2c15fd084d2dc2
Domenico Prestia <domenicoprestiaa@hotmail.com>
2021-04-14 14:42:51 +0200

    Main page completed

--- views/js/main.js
+++ views/js/main.js
@@ -1,17 +1,24 @@
 let loggedUser = sessionStorage.getItem('logged')
 let restaurantContainer = document.getElementById('container')
+let platesContainer = document.getElementById('plates')
 
 displayRestaurants()
 let restaurants = document.getElementsByClassName('rest')
 
-Array.from(restaurants).forEach(element => {
-   element.addEventListener('click', event => {
+Array.from(restaurants).forEach( element => {
+   element.addEventListener('click',async event => {
       let element = event.target
+      platesContainer.innerHTML = ''
 
       if(!String(event.target).includes('Div')) element = event.target.parentElement
 
-      console.log(element.children)
+      let username = element.children[3].innerHTML
+      
+      let makers = await JSON.parse(localStorage.getItem('users'))
 
+      makers.forEach(maker => {if(maker.username == username)
+         {platesContainer.innerHTML = `<h1 class="text-4xl mt-4 text-purple-600 text-center">${maker.restaurant.nome}</h1>`; 
+      displayPlates(maker.restaurant.piatti_ordinabili)}})
    });
  });
 
@@ -45,6 +52,16 @@ slider.addEventListener('mousemove', (e) => {
 
 //#endregion
 //#region functions 
+function displayPlates(plates){
+   plates.forEach(plate => {
+      platesContainer.innerHTML += `
+      <div class="flex mt-10 mx-10">
+         <p class="flex-1 text-2xl">${plate.nome_piatto}, ${plate.prezzo}€</p>
+         <button class="bg-purple-600 hover:bg-purple-800 text-white font-bold px-2 py-1 rounded mb-2" id="addBtn">Add</button>
+      </div>
+      <hr class="mx-10">`
+   })
+}
 
 function displayRestaurants(){
    let data = JSON.parse(localStorage.getItem('users'))
@@ -61,7 +78,7 @@ function displayRestaurants(){
                   <h5 class="card-title text-2xl" id='name'}>${maker.restaurant.nome}</h5>
                      <h6 class="card-subtitle mb-2 text-muted">${maker.restaurant.address}</h6>
                   <p class="card-text">${maker.restaurant.description}</p>
-                  <p class="card-text" id="username">${maker.username}</p>
+                  <p class="card-text" id="user">${maker.username}</p>
             </div>
          </div>`
    restaurantContainer.innerHTML += card
