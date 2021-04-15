ac3c0be8afe9bbb850fb4bb9a754ca118c62bc09
Domenico Prestia <domenicoprestiaa@hotmail.com>
2021-04-15 08:06:42 +0200

    Progressing with checkout page

--- views/js/checkout.js
+++ views/js/checkout.js
@@ -10,8 +10,14 @@ displayOrders()
 //#region functions
 function displayOrders(){
    let ordersArr = JSON.parse(sessionStorage.getItem('checkout'))
-   ordersArr.forEach(element => {
-      
+   ordersArr.forEach(ordine => {
+      orders.innerHTML += `
+      <div class="flex mt-10 mx-10">
+         <p class="flex-1 text-2xl">${ordine.name}</p>
+         <p class="w-1/6 text-l text-gray-600">${ordine.minutes}</p>
+         <p class="w-1/10 mr-5 text-2xl">${ordine.price}</p>
+      </div>
+      <hr class="mx-10">`
    });
 }
 //#endregion
