d005ab437f5f346440074e2f91be030d14e6e28a
Domenico Prestia <domenicoprestiaa@hotmail.com>
2021-04-19 22:00:32 +0200

    Last responsive changes

--- views/js/modifyUser.js
+++ views/js/modifyUser.js
@@ -187,7 +187,7 @@ modifyButton.addEventListener('click', () => {
         } else { error.innerHTML = 'Please fill every single field' }
     }
 
-const formLInfos = `<div class="bg-grey-lighter min-h-screen flex flex-col wrapper-registration" style="width: 500px;">
+const formLInfos = `<div class="bg-grey-lighter min-h-screen flex flex-col" style="width: 500px;">
 <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
     <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
         <h1 class="mb-8 text-3xl text-center">Sign up food-lover!</h1>
